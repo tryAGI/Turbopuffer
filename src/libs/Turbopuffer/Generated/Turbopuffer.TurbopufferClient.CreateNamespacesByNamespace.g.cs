a8a39c47ffa47d0855b59d76459f40fc08728426
Konstantin S. <havendv@gmail.com>
2026-04-01 09:16:22 +0400

    feat: Updated OpenAPI spec (#12)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Turbopuffer/Generated/Turbopuffer.TurbopufferClient.CreateNamespacesByNamespace.g.cs
+++ src/libs/Turbopuffer/Generated/Turbopuffer.TurbopufferClient.CreateNamespacesByNamespace.g.cs
@@ -298,7 +298,6 @@ namespace Turbopuffer
             bool? disableBackpressure = default,
             global::System.Threading.CancellationToken cancellationToken = default)
         {
-
             var __request = new global::Turbopuffer.Write
             {
                 UpsertColumns = upsertColumns,
