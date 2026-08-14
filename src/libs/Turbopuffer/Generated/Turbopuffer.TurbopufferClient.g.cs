bb4f6fe7d55c5ab69db6d53c376785db95c6d9f7
Konstantin S. <havendv@gmail.com>
2026-08-14 19:40:52 +0400

    feat: Updated OpenAPI spec (#42)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Turbopuffer/Generated/Turbopuffer.TurbopufferClient.g.cs
+++ src/libs/Turbopuffer/Generated/Turbopuffer.TurbopufferClient.g.cs
@@ -34,10 +34,17 @@ namespace Turbopuffer
 
         /// <inheritdoc/>
         public global::Turbopuffer.AutoSDKClientOptions Options { get; }
+
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Turbopuffer.SourceGenerationContext.Default);
+
         /// <summary>
         /// 
         /// </summary>
-        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext { get; set; } = global::Turbopuffer.SourceGenerationContext.Default;
+        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext
+        {
+            get => JsonSerializerContextProvider.Value;
+            set => JsonSerializerContextProvider = new(() => value);
+        }
 
 
         /// <summary>
